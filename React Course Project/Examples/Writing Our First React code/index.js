855479bf09c55c7064f8e3797c17272b7e334920
mitanshubhavsar <64788813+mitanshubhavsar@users.noreply.github.com>
2020-12-16 20:02:15 +0530

    Updated

---
+++ React Course Project/Examples/Writing Our First React code/index.js
@@ -0,0 +1,19 @@
+//working in codepen
+
+function Person(props) {
+  return (
+    <div class="person">
+      <h1>{props.name}</h1>
+      <p>Your Age: {props.age} </p>
+    </div>
+  );
+}
+
+var app = (
+  <div>
+    <Person name="Max" age="28" />
+    <Person name="Manu" age="29" />
+  </div>
+);
+
+ReactDOM.render(app, document.querySelector('#app'));
